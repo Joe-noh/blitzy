389f1118e93c9b4d20e4d2ee1aa4bc23e4765f3b
Joe-noh <goflb.jh@gmail.com>
2018-01-20 16:20:21 +0900

    not implemented yet

--- mix.exs
+++ mix.exs
@@ -14,7 +14,6 @@ defmodule Blitzy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Blitzy, []},
       extra_applications: [:logger]
     ]
   end
